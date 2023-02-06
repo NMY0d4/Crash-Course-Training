1f1b394fe1380e088a3a4d45ca0c1e228e37e8d2
Grégory Marini <greg.marini@hotmail.fr>
2023-02-06 18:36:43 +0100

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import { Fragment, useState } from "react";
 import CategoryFilter from "./components/CategoryFilter";
-import Counter from "./components/Counter";
 import FactList from "./components/FactList";
+import Header from "./components/Header";
 import NewFactForm from "./components/NewFactForm";
 import "./style.css";
 
 const App = () => {
     const [showForm, setShowForm] = useState(false);
-    const appTitle = "Today I Learned";
+
     return (
         <Fragment>
-            {/* HEADER */}
-            <header className="header">
-                <div className="logo">
-                    <img src="logo.png" alt="logo" />
-                    <h1>{appTitle}</h1>
-                </div>
-                <button
-                    className="btn btn-large btn-open"
-                    onClick={() => setShowForm((show) => !show)}
-                >
-                    Share a fact
-                </button>
-            </header>
+            <Header showForm={showForm} setShowForm={setShowForm} />
             {showForm ? <NewFactForm /> : null}
+
             <main className="main">
                 <CategoryFilter />
                 <FactList />
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+const appTitle = "Today I Learned";
+
+const Header = ({ showForm, setShowForm }) => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src="logo.png" alt="logo" />
+                <h1>{appTitle}</h1>
+            </div>
+            <button
+                className="btn btn-large btn-open"
+                onClick={() => setShowForm((show) => !show)}
+            >
+                {showForm ? "Close" : "Share a fact"}
+            </button>
+        </header>
+    );
+};
+
+export default Header;
